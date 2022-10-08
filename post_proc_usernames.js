fdd6822b2e9998bcedfd9011b95b02c179de0913
bart <hyperbart@protonmail.com>
2022-10-09 00:49:37 +1300

    new username patch

--- post_proc_usernames.js
+++ post_proc_usernames.js
@@ -4,6 +4,9 @@ const usernames = require('./usernames/usernames.json')
 delete usernames["0ALEC STEIMEL"];
 usernames["ALEC STEIMEL"] = ["ALEC STEIMEL"]
 
+delete usernames[".00CHARLIE VIELMA"];
+usernames["CHARLIE VIELMA"] = ["CHARLIE VIELMA"]
+
 fs.writeFile(`./usernames/usernames.json`, JSON.stringify(usernames, null, 2), (err) => {
     console.log('usernames patched');
 });

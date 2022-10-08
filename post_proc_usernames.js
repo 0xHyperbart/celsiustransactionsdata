db0a53e58aef5d24cf5ffb42ec827d1defd30ff6
bart <hyperbart@protonmail.com>
2022-10-09 01:26:38 +1300

    new username patch

--- post_proc_usernames.js
+++ post_proc_usernames.js
@@ -12,6 +12,8 @@ usernames["DIDIER FONTAINE"] = ["DIDIER FONTAINE"]
 
 usernames["INVICTUS CAPITAL FINANCIAL TECHNOLOGIES SPC"] = ["INVICTUS CAPITAL FINANCIAL TECHNOLOGIES SPC"]
 
+usernames["RICHARD H. MONTGOMERY III IRREVOCABLE TRUST FBO WILLIAM J. MONTGOMERY U/A/D 8/15/2008 TR"] = ["RICHARD H. MONTGOMERY III IRREVOCABLE TRUST FBO WILLIAM J. MONTGOMERY U/A/D 8/15/2008 TR"]
+
 fs.writeFile(`./usernames/usernames.json`, JSON.stringify(usernames, null, 2), (err) => {
     console.log('usernames patched');
 });

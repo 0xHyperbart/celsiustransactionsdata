0567cc0c94ec3f8f3c92863b246f46807886297c
bart <hyperbart@protonmail.com>
2022-10-09 06:59:58 +1300

    switch to transaction based bulk inserting

--- create_sqlite_db.js
+++ create_sqlite_db.js
@@ -2,6 +2,10 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./db.sqlite3');
 
 db.serialize(() => {
+    db.run("BEGIN TRANSACTION");
+
+    db.run("DROP TABLE IF EXISTS transactions");
+
     db.run(`CREATE TABLE transactions (
         username TEXT,
         address TEXT,
@@ -14,23 +18,35 @@ db.serialize(() => {
         coin_quantity TEXT,
         coin_usd TEXT
     )`);
+    db.run("COMMIT");
 
-    const stmt = db.prepare("INSERT INTO transactions VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-    for (let i = 47; i <= 14384; i++) {
+    db.run("BEGIN TRANSACTION");
+
+    for (let i = 47; i <= 47; i++) {
         const pageNum = i - 46
         const page = require(`./json-pass-3/coin_transactions_${pageNum}.json`);
         for(let i = 0; i < page.length; i++) {
             const row = page[i]
-            stmt.run(row.username, row.address, row.username + row.address, row.date, row.account, row.type, row.descriptive_purpose, row.coin, row.coinQuantity, row.coinUSD);
+            const {address, date, account, type, descriptivePurpose, coin, coinQuantity} = row
+            const username = JSON.stringify(row.username)
+            const coinUSD = row.coinUSD || ""
+            const username_address = row.username.join('') + address
+            db.run(
+                `INSERT INTO transactions (
+                    username,
+                    address,
+                    username_address,
+                    date,
+                    account,
+                    type,
+                    descriptive_purpose,
+                    coin,
+                    coin_quantity,
+                    coin_usd
+                ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`, username, address, username_address, date, account, type, descriptivePurpose, coin, coinQuantity, coinUSD);
         }
     }
-
-
-    stmt.finalize();
-
-    // db.each("SELECT rowid AS id, info FROM lorem", (err, row) => {
-    //     console.log(row.id + ": " + row.info);
-    // });
+    db.run("COMMIT");
 });
 
 db.close();
